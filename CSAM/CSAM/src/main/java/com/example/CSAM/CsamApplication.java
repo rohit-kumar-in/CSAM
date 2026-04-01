8401db55be05d581eb544662552cbf825c1b3276
Rohit Kumar <52185804+rohit-kumar-in@users.noreply.github.com>
2026-04-01 09:46:06 +0530

    Add files via upload

---
+++ CSAM/CSAM/src/main/java/com/example/CSAM/CsamApplication.java
@@ -0,0 +1,13 @@
+package com.example.CSAM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsamApplication.class, args);
+	}
+
+}
